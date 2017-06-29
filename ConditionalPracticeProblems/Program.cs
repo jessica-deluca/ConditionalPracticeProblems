25716507df112951600c210b7157469210b82492
Your Name <you@example.com>
2017-06-28 22:04:56 -0400

    added practice questions

--- ConditionalPracticeProblems/Program.cs
+++ ConditionalPracticeProblems/Program.cs
@@ -10,6 +10,108 @@ namespace ConditionalPracticeProblems
     {
         static void Main(string[] args)
         {
+            //Console.WriteLine("Enter an integer.");
+            //int number1 = int.Parse(Console.ReadLine());
+
+            //Console.WriteLine("Enter a second integer.");
+            //int number2 = int.Parse(Console.ReadLine());
+
+            //if (number1 == number2)
+            //{
+            //    Console.WriteLine("The numbers " + number1 + " and " + number2 + " are the same number.");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("The numbers " + number1 + " and " + number2 + " are not equal to each other.");
+            //}
+
+            //Console.WriteLine("Enter a whole number.");
+            //int numberEntered = int.Parse(Console.ReadLine());
+
+            //if (numberEntered % 2 == 0)
+            ///*modulus operator finds remainder
+            // * if numberEntered divided by 2 has a remainder, the remainder does not equal 0 so the numberEntered is an odd number
+            // * if numberEntered divided by 2 does not have a remainder, the remainder is 0 so the numberEntered is even*/
+            //{
+            //    Console.WriteLine(numberEntered + " is an even number.");
+            //}
+            //else
+            //{
+            //    Console.WriteLine(numberEntered + " is an old number.");
+            //}
+
+            //Console.WriteLine("Enter either a positive or negative whole number.");
+            //int numberEntered = int.Parse(Console.ReadLine());
+
+            //if (numberEntered > 0)
+            //{
+            //    Console.WriteLine("The number " + numberEntered + " is a positive number.");
+            //}
+            //else if (numberEntered < 0)
+            //{
+            //    Console.WriteLine("The number " + numberEntered + " is a negative number.");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("0 is neither a positive or negative number.");
+            //}
+
+            //Console.WriteLine("Enter a letter.");
+            //char letterEntered = char.Parse(Console.ReadLine());
+
+            //switch(letterEntered)
+            //{
+            //    case 'a': //user can enter uppercase (A) or lowercase (a) and they still get correct response
+            //        Console.WriteLine("The letter " + letterEntered + " is a vowel.");
+            //        break;
+            //    case 'e':
+            //        Console.WriteLine("The letter " + letterEntered + " is a vowel.");
+            //        break;
+            //    case 'i':
+            //        Console.WriteLine("The letter " + letterEntered + " is a vowel.");
+            //        break;
+            //    case 'o':
+            //        Console.WriteLine("The letter " + letterEntered + " is a vowel.");
+            //        break;
+            //    case 'u':
+            //        Console.WriteLine("The letter " + letterEntered + " is a vowel.");
+            //        break;
+            //    default:
+            //        Console.WriteLine("The letter " + letterEntered + " is a consonant.");
+            //        break;
+            //}
+
+            //Console.WriteLine("Enter an integer.");
+            //int integer1 = int.Parse(Console.ReadLine());
+
+            //Console.WriteLine("Enter a second integer.");
+            //int integer2 = int.Parse(Console.ReadLine());
+
+            //if (integer1 > integer2)
+            //{
+            //    Console.WriteLine(integer1 + " is the biggest number.");
+            //}
+            //else
+            //{
+            //    Console.WriteLine(integer2 + " is the biggest number.");
+            //}
+
+            //Console.WriteLine("Enter an integer.");
+            //int integer1 = int.Parse(Console.ReadLine());
+
+            //Console.WriteLine("Enter a second integer.");
+            //int integer2 = int.Parse(Console.ReadLine());
+
+            //Console.WriteLine("Enter a third integer.");
+            //int integer3 = int.Parse(Console.ReadLine());
+
+            //Console.WriteLine("Enter a fourth integer.");
+            //int integer4 = int.Parse(Console.ReadLine());
+
+            //float sum = integer1 + integer2 + integer3 + integer4;
+
+            //Console.WriteLine(sum / 4 + " is the mean of your integers.");
+
         }
     }
 }
